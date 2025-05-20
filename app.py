38502d4e7834605c80d461840660d038bbfc96ff
Sean <seanpaulramos17@gmail.com>
2025-05-20 09:10:48 +0800

    feat: :sparkles: /api/chat integrated

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+# Optional: for checking if server is alive
+@app.route('/', methods=['GET'])
 def index():
-    return '<h1>Good Day!</h1>'
+    return 'Server is running!'
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    message = data.get('message', '').lower()
+
+    if message == 'hi':
+        response = 'Hello, Sean'
+    else:
+        response = "I didn't understand that."
+
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
